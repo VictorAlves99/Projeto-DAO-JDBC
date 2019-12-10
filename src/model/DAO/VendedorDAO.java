37bb47238279e60d5fc12527e5beee4037c0f61d
Victor Oliveira <vi_matheus1999@hotmail.com>
2019-12-10 09:52:51 -0300

    Interfaces DAO criadas

---
+++ src/model/DAO/VendedorDAO.java
@@ -0,0 +1,15 @@
+package model.DAO;
+
+import java.util.List;
+
+import entities.Vendedor;
+
+public interface VendedorDAO {
+
+	void insert(Vendedor obj);
+	void update(Vendedor obj);
+	void deleteById(Integer id);
+	Vendedor findById(Integer id);
+	List<Vendedor> findAll();
+	
+}
