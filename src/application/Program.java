c5af8de1e06a72bde3cd9f703a1a15fbfe8ee184
Victor Oliveira <vi_matheus1999@hotmail.com>
2019-12-10 09:48:28 -0300

    Vendedor criado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
+import java.util.Date;
+
+import entities.Departamento;
+import entities.Vendedor;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Departamento obj = new Departamento(1,"Jorge");
 		
+		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(vendedor);
 		
 	}
 	
---
+++ src/entities/Vendedor.java
@@ -0,0 +1,109 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Vendedor implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nome;
+	private String email;
+	private Date birthDate;
+	private double baseSalary;
+	
+	private Departamento departamento;
+	
+	public Vendedor() {
+		
+	}	
+
+	public Vendedor(int id, String nome, String email, Date birthDate, double baseSalary, Departamento departamento) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.departamento = departamento;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vendedor other = (Vendedor) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Vendedor [id=" + id + ", nome=" + nome + ", email=" + email + ", birthDate=" + birthDate
+				+ ", baseSalary=" + baseSalary + ", departamento=" + departamento + "]";
+	}
+	
+	
+}
